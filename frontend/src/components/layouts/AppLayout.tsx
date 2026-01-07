a929918e384cd5cd022c50148b9afd66c2eb1ffd
rlown <128786877+rlown@users.noreply.github.com>
2026-01-07 12:59:57 +0900

    [#1] feat : AppLayout 기본 구조 생성

---
+++ frontend/src/components/layouts/AppLayout.tsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+
+interface AppLayoutProps {
+  children: React.ReactNode;
+}
+
+const AppLayout = ({ children }: AppLayoutProps) => {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default AppLayout;
