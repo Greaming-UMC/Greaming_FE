ecd548f96e88c2ee205658ec571c4733f79b7cfa
rlown <128786877+rlown@users.noreply.github.com>
2026-01-07 12:59:10 +0900

    [#1] feat : Header 기본 구조 생성

---
+++ frontend/src/components/layouts/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-14 flex items-center px-6 border-b border-zinc-800">
+      {/* 로고 / 타이틀 영역 */}
+    </header>
+  );
+};
+
+export default Header;
