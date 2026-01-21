ec32c87f12bd486a8d76bfbafcbbc51f3507ce77
harrysjuns <harrysjuns@gachon.ac.kr>
2026-01-22 04:47:36 +0900

    Feat: add new SVG icons and update Vite configuration for SVG handling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
+const monoColor = '**/assets/icon/mono/*.svg?react'
+const multiColor = [
+  '**/assets/icon/multi*.svg?react',
+  '**/assets/logo/*.svg?react',
+]
+
 export default defineConfig({
   plugins: [
-    svgr({ svgrOptions: { icon: true } }),
+    svgr({
+      include: monoColor,
+      svgrOptions: {
+        icon: true,
+        replaceAttrValues: { '#121315': 'currentColor' },
+      },
+    }),
+    svgr({
+      include: multiColor,
+      svgrOptions: { icon: true },
+    }),
     tailwindcss(),
   ],
 })
