67a79ce601158892560fc782f1ea08c4e84b7e0d
harrysjuns <harrysjuns@gachon.ac.kr>
2026-01-29 01:02:14 +0900

    feat: ProfilePage 구조 설계 및  뷰 레이아웃 정의

---
+++ frontend/src/features/profile/ui/ProfileDashboard.tsx
@@ -0,0 +1,10 @@
+const ProfileDashboard = () => {
+
+    return (
+        <div className="mx-auto w-fit flex gap-[40px]">
+            {/* Dashboard */}
+            {/* Content */}
+        </div>
+    )
+
+}; export default ProfileDashboard;
---
+++ frontend/src/features/profile/ui/ProfileView.tsx
@@ -0,0 +1,10 @@
+const ProfileView = () => {
+
+    return (
+        <div className="mx-auto w-fit flex gap-[40px]">
+            {/* Dashboard */}
+            {/* Content */}
+        </div>
+    )
+
+}; export default ProfileView;
