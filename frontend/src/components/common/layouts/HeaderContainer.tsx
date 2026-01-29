d270b258bf2c924e037c4fcc4533dd0332e6572c
rlown <128786877+rlown@users.noreply.github.com>
2026-01-29 22:37:56 +0900

    [#36] chore : 주석 추가

--- frontend/src/components/common/layouts/HeaderContainer.tsx
+++ frontend/src/components/common/layouts/HeaderContainer.tsx
@@ -16,6 +16,7 @@ const HeaderContainer = ({ variant }: HeaderContainerProps) => {
     console.log('logout');
   };
 
+  // 스웨거 나오면 API관련해서 리팩토링 할 예정입니다....!
   return (
     <Header
       {...({ variant, userInfo, onLogout: handleLogout } as any)}
