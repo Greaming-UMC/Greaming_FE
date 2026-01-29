6f33181e527ba76849225c61dbcf51aa74b21fa9
rlown <128786877+rlown@users.noreply.github.com>
2026-01-29 16:03:47 +0900

    [#36] refactor: 헤더 기본 높이(62px) 기준으로 AppLayout 정리

--- frontend/src/components/common/layouts/AppLayout.tsx
+++ frontend/src/components/common/layouts/AppLayout.tsx
@@ -1,11 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 
 const AppLayout = () => {
+  const { pathname } = useLocation();
+  const isHome = pathname === '/';
+
   return (
     <div className="min-h-screen bg-transparent">
-      <Header />
-      <main>
+      <Header variant={isHome ? 'home' : 'default'} />
+      <main
+        className={
+          isHome
+            ? '' // 홈 : 헤더 겹침
+            : 'pt-[62px]' // 나머지 : 고정 헤더 높이만큼 패딩 추가
+        }
+      >
         <Outlet />
       </main>
     </div>
