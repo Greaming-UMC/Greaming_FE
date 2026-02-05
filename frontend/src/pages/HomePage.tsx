b7bd2985b88af46f2dbebfdcadadf4d0bdc8bf2e
rlown <128786877+rlown@users.noreply.github.com>
2026-02-05 23:15:43 +0900

    [#60] feat : 홈페이지 조립

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,56 @@
-import HomeCardGrid from "../features/home/components/HomeCardGrid";
+import { useMemo, useState } from "react";
+import PageContainer from "../components/common/layouts/PageContainer";
+import CardControls from "../features/home/components/controls/CardControls";
+import CardGrid from "../features/home/components/card/CardGrid";
+import HeroSection from "../features/home/components/hero/HeroSection";
+import type { ChallengeType, HomeView } from "../features/home/components/type";
+
+import HomeBg from "../assets/background/home_bg.svg";
+import {
+  DAILY_CHALLENGE_CARDS,
+  WEEKLY_CHALLENGE_CARDS,
+} from "../features/home/api/MockHomeChallengeCards";
 
 const HomePage = () => {
+  const [view, setView] = useState<HomeView>("HOME");
+
+  const handleJoin = (type: ChallengeType) => {
+    setView(type); // "DAILY" | "WEEKLY"
+  };
+
+  const dailyCards = useMemo(() => DAILY_CHALLENGE_CARDS, []);
+  const weeklyCards = useMemo(() => WEEKLY_CHALLENGE_CARDS, []);
+
   return (
-    
+    <>
+      <section
+        className="w-full bg-primary bg-no-repeat bg-top"
+        style={{
+          backgroundImage: `url(${HomeBg})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <PageContainer>
+          <div className="pt-14 pb-10">
+            <HeroSection
+              view={view}
+              onJoin={handleJoin}
+              dailyCards={dailyCards}
+              weeklyCards={weeklyCards}
+            />
+          </div>
+        </PageContainer>
+      </section>
 
-
-    <HomeCardGrid/>
+      <section className="w-full bg-surface">
+        <PageContainer>
+          <div className="mx-auto w-full max-w-[1366px]">
+            <CardControls view={view} />
+            <CardGrid />
+          </div>
+        </PageContainer>
+      </section>
+    </>
   );
 };
 
