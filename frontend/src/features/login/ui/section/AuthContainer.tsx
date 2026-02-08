7898a2c8b1984f4b60f12bf1e4b503cde5b5ab02
harrysjuns <harrysjuns@gachon.ac.kr>
2026-02-08 16:46:26 +0900

    feat: add page nation

--- frontend/src/features/login/ui/section/AuthContainer.tsx
+++ frontend/src/features/login/ui/section/AuthContainer.tsx
@@ -1,10 +1,12 @@
-// AuthContainer.tsx
+import { useNavigate } from "react-router-dom";
 import Logo from "../../../../components/common/Logo";
 import { Button } from "../../../../components/common/input";
 import { Divider } from "../../../../components/common";
 import SocialLoginButtons from "../../components/SocialLoginButtons";
 
 const AuthContainer = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="relative flex justify-center w-[900px] h-[684px] bg-building-block">
         <div className="flex flex-col w-[320px] items-center justify-center">
@@ -13,7 +15,14 @@ const AuthContainer = () => {
                 그리고 · 공유하고 · 성장하는 커뮤니티
             </p>
 
-            <Button variant="onPrimary" size="md" shape="round" widthMode="fill" className="mt-10">
+            <Button
+              variant="onPrimary"
+              size="md"
+              shape="round"
+              widthMode="fill"
+              className="mt-10"
+              onClick={() => navigate("/home")}
+            >
                 그리밍 둘러보러가기
             </Button>
 
