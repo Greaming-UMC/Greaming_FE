c7c73d130a5919f9be958c4c639f2674252f9d91
hexter31376 <zwonyoung33@gmail.com>
2026-02-09 23:18:28 +0900

    [#75] refactor : 소셜 로그인 엔드포인트 변경 및 주석 수정

--- frontend/src/features/login/api/api.ts
+++ frontend/src/features/login/api/api.ts
@@ -2,7 +2,7 @@ import { ENDPOINTS } from "../../../libs/http/endpoints/endpoints";
 import type { SocialProvider } from "../config/types";
 
 export const buildSocialLoginUrl = (provider: SocialProvider) => {
-  const path = ENDPOINTS.AUTH.SOCIAL_LOGIN(provider); // /api/auth/login/social/{provider}
+  const path = ENDPOINTS.AUTH.SOCIAL_LOGIN(provider); // /oauth2/authorization/{provider}
   const base = import.meta.env.VITE_API_BASE_URL; // 예: http://13.124.68.142:8081
 
   if (!base) return path;
