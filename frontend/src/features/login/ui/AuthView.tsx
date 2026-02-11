3f0e5a19a3903291d565fc6f33427ad5e4a062a6
harrysjuns <harrysjuns@gachon.ac.kr>
2026-02-12 00:08:53 +0900

    fix: adjust padding in AuthView component

--- frontend/src/features/login/ui/AuthView.tsx
+++ frontend/src/features/login/ui/AuthView.tsx
@@ -3,7 +3,7 @@ import AuthContainer from "./section/AuthContainer";
 const AuthView = () => {
     return (
         <div className="bg-auth-onboarding min-h-dvh w-full">
-            <div className="flex justify-center px-4 pt-[150px]">
+            <div className="flex justify-center px-4 pt-60">
                 <AuthContainer />
             </div>
         </div>
