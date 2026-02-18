101d27f48810a24407cd06f99e08840a42b05407
rlown <128786877+rlown@users.noreply.github.com>
2026-02-18 15:48:54 +0900

    [#98] style : 상단 글자 크기 및 두께 조절

--- frontend/src/features/home/ui/hero/HeroHeaderText.tsx
+++ frontend/src/features/home/ui/hero/HeroHeaderText.tsx
@@ -8,8 +8,8 @@ const HeroHeaderText = ({ view }: Props) => {
   if (view === "HOME") {
     return (
       <p className="text-center text-on-primary mb-10 leading-snug animate-fadeIn">
-        <span className="text-lg">매일, 매주 하나의 주제로 그리는 기록,&nbsp;</span>
-        <span className="text-2xl font-bold">그리밍 챌린지</span>
+        <span className="text-2xl font-semibold">매일, 매주 하나의 주제로 그리는 기록,&nbsp;</span>
+        <span className="text-3xl font-bold">그리밍 챌린지</span>
       </p>
     );
   }
